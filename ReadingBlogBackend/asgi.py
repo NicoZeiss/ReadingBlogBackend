cc7f6a37e0c13e514d6c10bace6b704041c1db74
NicoZeiss <zeissern@gmail.com>
2024-03-14 00:36:55 +0100

    rename project

--- ReadingBlogBackend/asgi.py
+++ ReadingBlogBackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ReadingBlogBakend project.
+ASGI config for ReadingBlogBackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReadingBlogBakend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReadingBlogBackend.settings')
 
 application = get_asgi_application()
