b4fdb43537a5eb05976ac0686e180790b2dbb608
NicoZeiss <zeissern@gmail.com>
2024-03-20 02:48:42 +0100

    Add migrations

--- rb_books/admin.py
+++ rb_books/admin.py
@@ -3,77 +3,94 @@ from django.contrib import admin
 from .models import Volume, Author, Editor, Audience, Genre, Rating, Collection, Book
 
 
-class VolumeAdmin(admin.ModelAdmin):
-    list_display = (
+class CustomModelAdmin(admin.ModelAdmin):
+    actions_on_bottom = True
+    actions_on_top = False
+
+    exclude = ['slug']
+
+
+class VolumeAdmin(CustomModelAdmin):
+    list_display = [
         'label', 'code',
-    )
+    ]
 
 
-class AuthorAdmin(admin.ModelAdmin):
+class AuthorAdmin(CustomModelAdmin):
     pass
 
 
-class EditorAdmin(admin.ModelAdmin):
+class EditorAdmin(CustomModelAdmin):
     pass
 
 
-class AudienceAdmin(admin.ModelAdmin):
-    list_display = (
+class AudienceAdmin(CustomModelAdmin):
+    list_display = [
         'label', 'short_label',
-    )
+    ]
 
 
-class GenreAdmin(admin.ModelAdmin):
+class GenreAdmin(CustomModelAdmin):
     pass
 
 
-class RatingAdmin(admin.ModelAdmin):
-    list_display = (
+class RatingAdmin(CustomModelAdmin):
+    list_display = [
         'label', 'rating',
-    )
+    ]
 
 
-class CollectionAdmin(admin.ModelAdmin):
-    list_display = (
+class CollectionAdmin(CustomModelAdmin):
+    list_display = [
         'img_preview', 'title', 'author', 'editor', 'audience', 'volumes_count', 'complete',
-    )
-    list_display_links = (
+    ]
+    list_display_links = [
         'img_preview', 'title',
-    )
-    readonly_fields = (
+    ]
+    readonly_fields = [
         'img_preview',
-    )
-    list_editable = (
+    ]
+    list_editable = [
         'complete',
-    )
-    list_filter = (
+    ]
+    list_filter = [
         'audience', 'complete',
-    )
-    search_fields = (
+    ]
+    search_fields = [
         'title', 'author__first_name', 'author__last_name', 'editor__name',
-    )
+    ]
 
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = (
+class BookAdmin(CustomModelAdmin):
+    # List parameters
+    list_display = [
         'img_preview', '__str__', 'author', 'editor', 'audience', 'rating', 'incoming_reading', 'current_reading',
         'published', 'published_at', 'created_at',
-    )
-    list_display_links = (
+    ]
+    list_display_links = [
         'img_preview', '__str__',
-    )
-    readonly_fields = (
-        'published_at', 'img_preview',
-    )
+    ]
     list_editable = (
         'rating', 'incoming_reading', 'current_reading', 'published',
     )
-    search_fields = (
+
+    # Create/Update forms
+    fields = [
+        'title', 'volume', 'collection', 'sub_title', 'author', 'editor', 'pages', 'price', 'category', 'genres',
+        'summary', 'opinion', 'short_opinion', 'quotation', 'rating', 'incoming_reading', 'current_reading',
+        'published', 'published_at', 'created_at', 'img_preview',
+    ]
+    readonly_fields = [
+        'published_at', 'created_at', 'img_preview',
+    ]
+
+    # Search/Filter
+    search_fields = [
         'title', 'sub_title', 'volume__label', 'author__first_name', 'author__last_name', 'editor__name',
-    )
-    list_filter = (
+    ]
+    list_filter = [
         'audience', 'rating', 'incoming_reading', 'current_reading', 'published', 'published_at',
-    )
+    ]
 
 
 admin.site.register(Volume, VolumeAdmin)
@@ -84,3 +101,8 @@ admin.site.register(Genre, GenreAdmin)
 admin.site.register(Rating, RatingAdmin)
 admin.site.register(Collection, CollectionAdmin)
 admin.site.register(Book, BookAdmin)
+
+# TODO: Dans Book supprimer le subtitle, et considérer que le title est le titre d'un volume. Si title vide, alors on
+#  copie title de collection
+# TODO: se renseigner sur l'app facultative FlatPages de Django
+# TODO: regarder fonctionnement des Validators (voir si ça peut servir pour valider titre/collection)
--- rb_books/utils.py
+++ rb_books/utils.py
@@ -5,48 +5,66 @@ from rb_books.models import Audience, Author, Book, Collection, Editor, Genre, R
 
 def create_sample_authors():
     for _ in range(10):
-        first_name, last_name = [e.capitalize() for e in lorem.words(2).split(' ')]
-        Author.objects.create(first_name=first_name, last_name=last_name)
+        try:
+            first_name, last_name = [e.capitalize() for e in lorem.words(2).split(' ')]
+            Author.objects.create(first_name=first_name, last_name=last_name)
+        except:
+            pass
 
 
 def create_sample_editors():
     for _ in range(10):
-        name = lorem.words(1)
-        Editor.objects.create(name=name)
+        try:
+            name = lorem.words(1)
+            Editor.objects.create(name=name)
+        except:
+            pass
 
 
 def create_sample_genres():
     for _ in range(20):
-        label_number = random.randint(1, 2)
-        label = lorem.words(label_number).capitalize()
-        description_number = random.randint(10, 50)
-        description = lorem.words(description_number)
-        Genre.objects.create(label=label, description=description)
+        try:
+            label_number = random.randint(1, 2)
+            label = lorem.words(label_number).capitalize()
+            description_number = random.randint(10, 50)
+            description = lorem.words(description_number)
+            Genre.objects.create(label=label, description=description)
+        except:
+            pass
 
 
 def create_sample_ratings():
     for i in range(6):
-        label_number = random.randint(1, 3)
-        label = lorem.words(label_number).capitalize()
-        code = label[:2].upper()
-        description_number = random.randint(10, 30)
-        description = lorem.words(description_number)
-        Rating.objects.create(label=label, code=code, description=description, rating=i)
+        try:
+            label_number = random.randint(1, 3)
+            label = lorem.words(label_number).capitalize()
+            code = label[:2].upper()
+            description_number = random.randint(10, 30)
+            description = lorem.words(description_number)
+            Rating.objects.create(label=label, code=code, description=description, rating=i)
+        except:
+            pass
 
 
 def create_sample_volumes():
     for i in range(100):
-        value = i + 1
-        Volume.objects.create(label=f'Tome {value}', code=f'T{value}')
+        try:
+            value = i + 1
+            Volume.objects.create(label=f'Tome {value}', code=f'T{value}')
+        except:
+            pass
 
 
 def create_sample_audiences():
     for i in range(6):
-        label_number = random.randint(2, 4)
-        label = lorem.words(label_number).capitalize()
-        description_number = random.randint(5, 20)
-        description = lorem.words(description_number)
-        Audience.objects.create(label=label, short_label=lorem.words(1), description=description)
+        try:
+            label_number = random.randint(2, 4)
+            label = lorem.words(label_number).capitalize()
+            description_number = random.randint(5, 20)
+            description = lorem.words(description_number)
+            Audience.objects.create(label=label, short_label=lorem.words(1), description=description)
+        except:
+            pass
 
 
 def create_sample_data():
